50807b3301fa8d691ff7de8a38dd897f0bbda7aa
Hannah Bourke <hannah25@gmail.com>
2017-12-06 11:40:30 +1100

    Initial commit

---
+++ api/server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+
+// const cors = require('cors')
+const server = express()
+
+
+// Plugins/Middleware
+server.use(bodyParser.json())
+// server.use(cors())
+
+// Routes
+// Pass any routes set in /rainfalls on the root directory
+server.use('/', [
+  require('../api/routes/rainfalls')
+])
+server.get('/', (req, res) => {
+  res.json({ message: "Work" })
+})
+
+
+// Start server
+server.listen(7000, (error) => {
+  if (error) {
+    console.log('Error starting', error)
+  } else {
+    console.log('Server started at http://localhost:7000/')
+  }
+})
